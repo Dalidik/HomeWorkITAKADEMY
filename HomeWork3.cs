2357e98cdf1ff7b740f208ce09d100dd3aadf98b
Dalidik <134883096+Dalidik@users.noreply.github.com>
2023-06-04 16:05:08 +0200

    Add files via upload

---
+++ HomeWork3.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+Console.WriteLine("please input W, A, S, D to move the object");
+
+var input = Console.ReadLine();
+
+switch (input)
+{
+    case "w":
+    Console.WriteLine("The object has been moved up!");
+        break;
+
+    case "a":
+    Console.WriteLine("The object has been moved to the left!");
+        break;
+
+    case "s":
+    Console.WriteLine("The object has been moved down!");
+        break;
+
+    case "d":
+    Console.WriteLine("The object has been moved right!");
+        break;
+
+    default:
+        Console.WriteLine("The object is not moving...");
+        break;
+}
+
