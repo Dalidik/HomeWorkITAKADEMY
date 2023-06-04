d2e0a60e25f1a532cc946f69c4c56b4a51980972
Dalidik <134883096+Dalidik@users.noreply.github.com>
2023-06-04 15:57:28 +0200

    HomeWork2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
- int a = 5;
- int b = 4;
- Console.WriteLine(int a+b);
+int i = 0;
+do
+{
+    i -= 20;
+    Console.WriteLine(i);
+}
+while (i > -100);
+
+
+
+
+
