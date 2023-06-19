6c4994489dae24e9ef4be1e7eff32df9aeb28363
Dalidik <134883096+Dalidik@users.noreply.github.com>
2023-06-19 18:10:07 +0200

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-var Employee1 = new Employee("Tom", "Ford", "Sergeevich", "Chicago", 2000, "Doctor surgeon");
-Console.WriteLine(Employee1.Info());
-Employee1.SolveProblem();
-Console.WriteLine();
-var Customer1 = new Customer("Melania", "Trump", "Knauss", 1970, "601 E Rollins St, Orlando", "fracture of the right arm" );
-Console.WriteLine(Customer1.Info());
-Customer1.SolveProblem();
