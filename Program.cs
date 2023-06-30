1c01c0282e2eb31378d22f61a5bdf7958c20ed96
Dalidik <134883096+Dalidik@users.noreply.github.com>
2023-06-30 22:29:45 +0200

    HWW7

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = new int[6];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine("Enter numbers[{0}]", i);
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+Array.Sort(numbers);
+Array.Reverse(numbers);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");    
+}
+
+Console.ReadLine();
