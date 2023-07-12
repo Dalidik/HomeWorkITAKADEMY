ba5efa7a47e49fbfc448c9cd529023367a19d617
Dalidik <134883096+Dalidik@users.noreply.github.com>
2023-07-12 16:31:15 +0200

    HW4(2)

---
+++ HW4(2)/Program.cs
@@ -0,0 +1,18 @@
+﻿
+var names = new List<string>(){ "Bella", "Kaleb", "Jakob"};
+
+names.Add("Charli");
+
+names.Sort();
+    foreach (var name in names)
+    {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+    Console.WriteLine();
+    names.Add("Renesme");
+
+     foreach (var name in names)
+     {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+     }
+     Console.WriteLine();
