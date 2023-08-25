98c82b5e25c55ad331ae720797877fbb8e5d5915
Dalidik <134883096+Dalidik@users.noreply.github.com>
2023-08-25 22:22:49 +0200

    kontrolnaja1

--- Program.cs
+++ Program.cs
@@ -1,17 +1,33 @@
-﻿int[] numbers = new int[6];
+﻿int rows = 5; 
+int cols = 5; 
 
-for (int i = 0; i < numbers.Length; i++)
-{
-    Console.WriteLine("Enter numbers[{0}]", i);
-    numbers[i] = int.Parse(Console.ReadLine());
-}
+int[,] array = new int[rows, cols];
 
-Array.Sort(numbers);
-Array.Reverse(numbers);
+Random random = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    array[i, j] = random.Next(1, 10); 
+                }
+            }
+             for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (j > i) 
+                    {
+                        array[i, j] = 1;
+                    }
+                }
+            }
 
-for (int i = 0; i < numbers.Length; i++)
-{
-    Console.Write(numbers[i] + " ");    
-}
-
-Console.ReadLine();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        
